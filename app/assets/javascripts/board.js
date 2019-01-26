2ba6d3d87612851a847a40b9267e8862a157d4a6
Alexey V.Ryapisov <alexey.ryapisov@yandex.ru>
2019-01-26 23:57:19 +0300

    refactor js border

--- app/assets/javascripts/board.js
+++ app/assets/javascripts/board.js
@@ -1,25 +1,30 @@
-function toggleClass(i){
-    var  elems = document.getElementsByClassName('cages');
-    var  elem  = document.getElementById(elems[i].getAttribute("id"));
-    var _class = elem.classList.toggle('light-cage');
-  return _class; 
-}
+var elems = document.getElementsByClassName('cages');
 
-window.onload = function() {
-  for (i=0; i < 64; i++){
-    
-    if (i % 2 == 0){   
-      }else if (i >=  0 && i <  8){ toggleClass(i); // 8 line
-      }else if (i >= 16 && i < 24){ toggleClass(i); // 6 line
-      }else if (i >= 32 && i < 40){ toggleClass(i); // 4 line
-      }else if (i >= 48 && i < 56){ toggleClass(i); // 2 line
+function addClassLight(i){ elems[i].classList.add('cage-light') };
+function addClassDark(i) { elems[i].classList.add('cage-dark')  };
+
+window.onload = function(){
+  for (let i=0; i < 64; i++){
+
+    if (i % 2 == 0){
+      }else if (i >=  0 && i <  8){ addClassDark(i);
+      }else if (i >=  8 && i < 16){ addClassLight(i);
+      }else if (i >= 16 && i < 24){ addClassDark(i);
+      }else if (i >= 24 && i < 32){ addClassLight(i);
+      }else if (i >= 32 && i < 40){ addClassDark(i);
+      }else if (i >= 40 && i < 48){ addClassLight(i);
+      }else if (i >= 48 && i < 56){ addClassDark(i);
+      }else if (i >= 56 && i < 64){ addClassLight(i);
     }
-
     if (i % 2 != 0){
-      }else if (i >= 8  && i < 16){ toggleClass(i); // 7 line
-      }else if (i >= 24 && i < 32){ toggleClass(i); // 5 line
-      }else if (i >= 40 && i < 48){ toggleClass(i); // 3 line
-      }else if (i >= 56 && i < 64){ toggleClass(i); // 1 line
-    } 
+      }else if (i >=  0 && i <  8){ addClassLight(i);
+      }else if (i >=  8 && i < 16){ addClassDark(i);
+      }else if (i >= 16 && i < 24){ addClassLight(i);
+      }else if (i >= 24 && i < 32){ addClassDark(i);
+      }else if (i >= 32 && i < 40){ addClassLight(i);
+      }else if (i >= 40 && i < 48){ addClassDark(i);
+      }else if (i >= 48 && i < 56){ addClassLight(i);
+      }else if (i >= 48 && i < 64){ addClassDark(i);
+    }
   }
 };
