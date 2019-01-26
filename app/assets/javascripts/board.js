f899c17c2c764403c5176e90906ddb7ea15fccab
Alexey V.Ryapisov <alexey.ryapisov@yandex.ru>
2019-01-27 00:04:38 +0300

    refactor js board

--- app/assets/javascripts/board.js
+++ app/assets/javascripts/board.js
@@ -7,24 +7,24 @@ window.onload = function(){
   for (let i=0; i < 64; i++){
 
     if (i % 2 == 0){
-      }else if (i >=  0 && i <  8){ addClassDark(i);
-      }else if (i >=  8 && i < 16){ addClassLight(i);
-      }else if (i >= 16 && i < 24){ addClassDark(i);
-      }else if (i >= 24 && i < 32){ addClassLight(i);
-      }else if (i >= 32 && i < 40){ addClassDark(i);
-      }else if (i >= 40 && i < 48){ addClassLight(i);
-      }else if (i >= 48 && i < 56){ addClassDark(i);
-      }else if (i >= 56 && i < 64){ addClassLight(i);
+      }else if (i <  8){ addClassDark(i);
+      }else if (i < 16){ addClassLight(i);
+      }else if (i < 24){ addClassDark(i);
+      }else if (i < 32){ addClassLight(i);
+      }else if (i < 40){ addClassDark(i);
+      }else if (i < 48){ addClassLight(i);
+      }else if (i < 56){ addClassDark(i);
+      }else if (i < 64){ addClassLight(i);
     }
     if (i % 2 != 0){
-      }else if (i >=  0 && i <  8){ addClassLight(i);
-      }else if (i >=  8 && i < 16){ addClassDark(i);
-      }else if (i >= 16 && i < 24){ addClassLight(i);
-      }else if (i >= 24 && i < 32){ addClassDark(i);
-      }else if (i >= 32 && i < 40){ addClassLight(i);
-      }else if (i >= 40 && i < 48){ addClassDark(i);
-      }else if (i >= 48 && i < 56){ addClassLight(i);
-      }else if (i >= 48 && i < 64){ addClassDark(i);
+      }else if (i <  8){ addClassLight(i);
+      }else if (i < 16){ addClassDark(i);
+      }else if (i < 24){ addClassLight(i);
+      }else if (i < 32){ addClassDark(i);
+      }else if (i < 40){ addClassLight(i);
+      }else if (i < 48){ addClassDark(i);
+      }else if (i < 56){ addClassLight(i);
+      }else if (i < 64){ addClassDark(i);
     }
   }
 };
